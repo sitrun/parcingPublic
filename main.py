591d3802c5008fdfc2abb587f917d569fd6abd01
ANDREI NOVOZHILOV <sitrun@users.noreply.github.com>
2022-05-19 15:28:53 +0700

    Set telethon keys

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
+import config
+from telethon import TelegramClient, sync
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name} name api{config.TG_API_KEY}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
